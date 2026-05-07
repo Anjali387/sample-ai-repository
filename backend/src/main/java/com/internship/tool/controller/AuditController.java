2b24b0dc88df05de35e80e9cef46f7d754968cfd
Anjali387 <your-anjalirgurav694@gmail.com>
2026-05-08 03:55:03 +0530

    Day 4 - Added REST Controller APIs

--- backend/src/main/java/com/internship/tool/controller/AuditController.java
+++ backend/src/main/java/com/internship/tool/controller/AuditController.java
@@ -1,4 +1,43 @@
 package com.internship.tool.controller;
 
+import com.internship.tool.entity.Audit;
+import com.internship.tool.service.AuditService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/audits")
 public class AuditController {
+
+    @Autowired
+    private AuditService auditService;
+
+    // CREATE AUDIT
+    @PostMapping
+    public Audit createAudit(@RequestBody Audit audit) {
+        return auditService.createAudit(audit);
+    }
+
+    // GET ALL AUDITS
+    @GetMapping
+    public List<Audit> getAllAudits() {
+        return auditService.getAllAudits();
+    }
+
+    // GET AUDIT BY ID
+    @GetMapping("/{id}")
+    public Audit getAuditById(@PathVariable Long id) {
+        return auditService.getAuditById(id);
+    }
+
+    // DELETE AUDIT
+    @DeleteMapping("/{id}")
+    public String deleteAudit(@PathVariable Long id) {
+
+        auditService.deleteAudit(id);
+
+        return "Audit deleted successfully";
+    }
 }
